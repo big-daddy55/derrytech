63d411a95761cf9b37bdabac2db1e22f1112b6b8
big-daddy <derrickazaglo123@gmail.com>
2024-07-01 01:54:34 +0000

    update our menu-items to use HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
 import { Homepage } from './pages/Homepage/Homepage.component';
+import { Route, Routes, } from 'react-router-dom';
 
+function Hats() {
+  return (
+    <div>Hats</div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <Routes>
+        <Route exact path="/" element={<Homepage/>} />
+        <Route exact path="/shop/hats" element={<Hats/>} />
+      </Routes>
+      {/* <Homepage/> */}
 
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useNavigate} from 'react-router-dom';
 import './menu-item.styles.scss'
 
-export const MenuItem = ({title, imageUrl, size}) => (
-    <div
+export const MenuItem = ({ title, imageUrl, size, linkUrl, }) => {
+
+    const navigate = useNavigate();
+    // const match = useMatch(pathPattern); // pathPattern is the pattern to match, e.g., '/some-path/:id'
+
+    const handleClick = () => {
+            navigate(linkUrl); // Or handle the case where there's no match
+        
+    };
+    
+    return (<div
         className={`${size} menu-item`}
+        onClick={handleClick}
     >
         <div
             style={{
@@ -16,4 +27,5 @@ export const MenuItem = ({title, imageUrl, size}) => (
             <span className="subtitle">Shop Now</span>
         </div>
     </div>
-)
+    )
+}
